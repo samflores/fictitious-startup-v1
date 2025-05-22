ef8c190fa943740c3970c933e09d56d56a70163e
Samuel Flores <me@samflor.es>
2025-05-22 09:37:48 -0300

    Create EC2 instance

---
+++ terraform/modules/computing/main.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "instance_sg" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+
+  associate_public_ip_address = true
+
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+
+  tags = {
+    Name = "WebServerInstance"
+  }
+}
+
