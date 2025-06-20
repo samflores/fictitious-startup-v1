0cd0aaa70ddcad2ddfa14d9afe32b5b92149713a
Samuel Flores <me@samflor.es>
2025-06-20 08:31:46 -0300

    Use AWS managed policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,10 +36,7 @@ data "aws_ami" "ubuntu" {
     values = ["ubuntu/images/*ubuntu-jammy-22.04-amd64-server*"]
   }
 
-  filter {
-    name   = "owner-id"
-    values = ["099720109477"]
-  }
+  owners = ["099720109477"]
 }
 
 resource "aws_iam_role" "ssm_role" {
@@ -62,58 +59,9 @@ resource "aws_iam_instance_profile" "ssm_profile" {
   role = aws_iam_role.ssm_role.name
 }
 
-resource "aws_iam_role_policy" "ssm_custom_policy" {
-  name = "ssm-custom-policy"
-  role = aws_iam_role.ssm_role.name
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "ssm:DescribeAssociation",
-          "ssm:GetDeployablePatchSnapshotForInstance",
-          "ssm:GetDocument",
-          "ssm:DescribeDocument",
-          "ssm:GetManifest",
-          "ssm:GetParameter",
-          "ssm:GetParameters",
-          "ssm:ListAssociations",
-          "ssm:ListInstanceAssociations",
-          "ssm:PutInventory",
-          "ssm:PutComplianceItems",
-          "ssm:PutConfigurePackageResult",
-          "ssm:UpdateAssociationStatus",
-          "ssm:UpdateInstanceAssociationStatus",
-          "ssm:UpdateInstanceInformation"
-        ],
-        Resource = "*"
-      },
-      {
-        Effect = "Allow",
-        Action = [
-          "ssmmessages:CreateControlChannel",
-          "ssmmessages:CreateDataChannel",
-          "ssmmessages:OpenControlChannel",
-          "ssmmessages:OpenDataChannel"
-        ],
-        Resource = "*"
-      },
-      {
-        Effect = "Allow",
-        Action = [
-          "ec2messages:AcknowledgeMessage",
-          "ec2messages:DeleteMessage",
-          "ec2messages:FailMessage",
-          "ec2messages:GetEndpoint",
-          "ec2messages:GetMessages",
-          "ec2messages:SendReply"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
+resource "aws_iam_role_policy_attachment" "ssm_managed_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.ssm_role.name
 }
 
 module "computing" {
