2e88f7634d215cb61d358af641cd91c3ead4b04d
Samuel Flores <me@samflor.es>
2025-06-22 16:02:58 -0300

    Use the private IPs list

--- terraform/modules/db_migration/output.tf
+++ terraform/modules/db_migration/output.tf
@@ -1,4 +1,4 @@
 output "address" {
   description = "The replicator instance IP"
-  value       = element(aws_dms_replication_instance.bootcamp_dms_instance.replication_instance_public_ips, 0)
+  value       = element(aws_dms_replication_instance.bootcamp_dms_instance.replication_instance_private_ips, 0)
 }
